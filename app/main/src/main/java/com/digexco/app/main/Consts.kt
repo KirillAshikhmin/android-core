0404140a085ad0db602692f82d9493f67da6bc2c
keklik2 <63215768+keklik2@users.noreply.github.com>
2022-04-14 13:39:42 +0300

    requested changes revision

--- app/main/src/main/java/com/digexco/app/main/Consts.kt
+++ app/main/src/main/java/com/digexco/app/main/Consts.kt
@@ -1,28 +1,2 @@
 package com.digexco.app.main
 
-object NetworkUrls {}
-
-object ArchConsts {
-    const val navigation_result_key = "navigation_result_key"
-    const val navigation_name = "navigation_name"
-}
-
-object FilesConst {
-    val BACKEND_SUPPORTED_TYPES = listOf(
-        "json",
-        "pdf",
-        "txt",
-        "csv",
-        "xls",
-        "xlsx",
-        "doc",
-        "docx",
-        "png",
-        "jpg",
-        "jpeg",
-        "gif",
-        "webp",
-        "zip"
-    )
-}
-
